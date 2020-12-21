6ea9ec5589dc8741a0128b1cef12d67e75c47e3b
Sascha Sternheim <sascha.sternheim@conti.de>
2020-12-21 15:08:57 +0100

    Removed ApplicationInsights

--- src/DeconzToMqtt/Program.cs
+++ src/DeconzToMqtt/Program.cs
@@ -7,7 +7,7 @@ namespace DeconzToMqtt
     class Program : Startup
     {
         static void Main(string[] args)
-            => new Program().Run(args, BootstrapInCodeConfiguration.Default()
-                                                                   .ConfigureTelemetry(x => x.InstrumentationKey = "30e4f76d-e462-4f0a-9f0a-9fbf4939558d"));
+            => new Program().Run(args, BootstrapInCodeConfiguration.Default());
+                                                                   //.ConfigureTelemetry(x => x.InstrumentationKey = "30e4f76d-e462-4f0a-9f0a-9fbf4939558d"));
     }
 }
