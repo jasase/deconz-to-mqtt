8ba46c876b08bec6db0b95923b754d5eaed0373f
Sascha Sternheim <sascha.sternheim@conti.de>
2021-05-22 21:30:55 +0200

    Fix

--- src/DeconzToMqtt/Mqtt/MqttClient.cs
+++ src/DeconzToMqtt/Mqtt/MqttClient.cs
@@ -114,7 +114,7 @@ namespace DeconzToMqtt.Mqtt
             });
 
             _logger.Info("Connecting to MQTT server '{0}'", _hostname);
-            _client.StartAsync(managedOptions.Build()).Wait(_cancelationToken.Token);
+            _client.StartAsync(managedOptions.Build()).Wait();
         }
 
         public bool Healthy()
